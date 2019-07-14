dbb5a1f78421085d191c2ea9fb8adec2722f7d32
Tomasz Gieraltowski <tgieraltowski@gmail.com>
2019-07-14 12:13:59 +0200

    Product factory created

--- src/main/java/org/sda/models/dao/Impl/ProductDaoImpl.java
+++ src/main/java/org/sda/models/dao/Impl/ProductDaoImpl.java
@@ -3,17 +3,24 @@ package org.sda.models.dao.Impl;
 import org.sda.datasource.Datasource;
 import org.sda.models.dao.ProductDao;
 import org.sda.models.dto.Product;
+import org.sda.models.factory.ProductFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import java.util.List;
 
 public class ProductDaoImpl implements ProductDao {
-    @Override
+    /*@Override
     public List<Product> getProductList() {
         EntityManager entityManager = Datasource.getEntityManager();
         List<Product> products = entityManager.createQuery("FROM Product p").getResultList();
         return products;
+    }*/
+
+    // temporary for development purposes only
+    @Override
+    public List<Product> getProductList() {
+        return ProductFactory.createProductList();
     }
 
     @Override
--- src/main/java/org/sda/models/factory/ProductFactory.java
+++ src/main/java/org/sda/models/factory/ProductFactory.java
@@ -1,5 +1,21 @@
 package org.sda.models.factory;
 
-public class ProductFactory {
+import org.sda.models.dto.Product;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductFactory {
+    public static List<Product> createProductList() {
+        Product product1 = new Product("XXL", "Nike Running Boots", "Blue", "M", 249.99, 10, 0);
+        Product product2 = new Product("S", "Sport Bra", "Black/Red", "F", 49.99, 15, 0);
+        Product product3 = new Product("M", "Nike T-Shirt", "White", "M", 59.50, 22, 0);
+        Product product4 = new Product("XL", "Adidas Leggins", "White/Black", "F", 84.99, 7, 0);
+        List<Product> products = new ArrayList<>();
+        products.add(product1);
+        products.add(product2);
+        products.add(product3);
+        products.add(product4);
+        return products;
+    }
 }
