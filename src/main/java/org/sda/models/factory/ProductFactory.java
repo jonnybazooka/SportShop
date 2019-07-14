68d55f5670895b7db190d4d54e34084f1e4bc3b0
Tomasz Gieraltowski <tgieraltowski@gmail.com>
2019-07-14 13:19:06 +0200

    Larger factory

--- src/main/java/org/sda/models/factory/ProductFactory.java
+++ src/main/java/org/sda/models/factory/ProductFactory.java
@@ -11,11 +11,19 @@ public class ProductFactory {
         Product product2 = new Product("S", "Sport Bra", "Black/Red", "F", 49.99, 15, 0);
         Product product3 = new Product("M", "Nike T-Shirt", "White", "M", 59.50, 22, 0);
         Product product4 = new Product("XL", "Adidas Leggins", "White/Black", "F", 84.99, 7, 0);
+        Product product5 = new Product("XLL", "Adidas T-Shirt", "White/Black", "F", 24.99, 16, 0);
+        Product product6 = new Product("M", "Reebok Socks", "Pink", "F", 14.00, 80, 0);
+        Product product7 = new Product("S", "Reebok Headband", "Red", "M", 22.50, 33, 0);
+        Product product8 = new Product("XS", "Adidas Socks", "White/Black", "M", 54.00, 80, 0);
         List<Product> products = new ArrayList<>();
         products.add(product1);
         products.add(product2);
         products.add(product3);
         products.add(product4);
+        products.add(product5);
+        products.add(product6);
+        products.add(product7);
+        products.add(product8);
         return products;
     }
 }
