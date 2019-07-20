d0e29e2890d92406e4ee8eadb3fbd83c9e16ed83
Tomasz Gieraltowski <tgieraltowski@gmail.com>
2019-07-20 13:34:11 +0200

    Basket send to mainView

--- src/main/java/org/sda/servlets/ViewServlet.java
+++ src/main/java/org/sda/servlets/ViewServlet.java
@@ -1,5 +1,7 @@
 package org.sda.servlets;
 
+import org.sda.models.dao.BasketDao;
+import org.sda.models.dao.Impl.BasketDaoImpl;
 import org.sda.models.dao.Impl.ProductDaoImpl;
 import org.sda.models.dto.Product;
 
@@ -15,11 +17,14 @@ public class ViewServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         List<Product> list = new ProductDaoImpl().getProductList();
-
+        BasketDao basketDao = new BasketDaoImpl();
+        if (req.getSession().getAttribute("name") != null) {
+            List<Product> basket = basketDao.getAllItems();
+            req.setAttribute("basket", basket);
+        }
         req.setAttribute("productList",list);
 
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("frontView.jsp");
-
         requestDispatcher.forward(req,resp);
     }
 }
